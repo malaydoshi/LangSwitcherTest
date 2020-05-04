b8355e6295ed215b7eba34cd1d8f6a0d6f3576f9
Malay Doshi <malaypdoshi@hotmail.com>
2020-05-04 12:08:12 +0530

    Add project files.

---
+++ clientapp/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+      <hr />
+      <label for="lang">Select Your Language: </label>
+      <select v-model="$i18n.locale" id="lang">
+          <option v-for="(lang,i) in $i18n.availableLocales" :key="`Lang${i}`" :value="lang">{{lang}}</option>
+      </select>
+      <hr />
+      <p>{{ $t('message') }}</p>
+      <hr />
+      <p>{{selectedLang}}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+    //HelloWorld
+        },
+        data() {
+            return {
+                //selectedLang: 'en',
+                //availableLang: ['en','fr','ja']
+            }
+        }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
